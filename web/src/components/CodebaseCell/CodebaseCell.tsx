5b9bb18f42e75dc6c80223e0e635ab6d52ea44f0
David Thyresson <dthyresson@gmail.com>
2024-08-06 10:55:22 -0400

    Format truncated code

--- web/src/components/CodebaseCell/CodebaseCell.tsx
+++ web/src/components/CodebaseCell/CodebaseCell.tsx
@@ -3,6 +3,7 @@ import type {
   FindCodebaseQueryVariables,
 } from 'types/graphql'
 
+import { Link, routes } from '@redwoodjs/router'
 import type {
   CellSuccessProps,
   CellFailureProps,
@@ -33,5 +34,28 @@ export const Failure = ({
 export const Success = ({
   codebase,
 }: CellSuccessProps<FindCodebaseQuery, FindCodebaseQueryVariables>) => {
-  return <Markdown>{codebase}</Markdown>
+  const truncatedCodebase = codebase.split('\n').slice(0, 70).join('\n')
+  return (
+    <>
+      <div className="my-4 text-center">
+        <Link
+          to={routes.viewFile({ path: `.rw-chatbot/CODEBASE_TOC.md` })}
+          className="text-sm font-semibold leading-6 text-green-600"
+        >
+          View Complete Codebase <span aria-hidden="true">→</span>
+        </Link>
+      </div>
+      <div className="my-4 rounded-lg border border-solid border-gray-200 bg-white p-12">
+        <Markdown>{truncatedCodebase}</Markdown>
+      </div>
+      <div className="mt-y text-center">
+        <Link
+          to={routes.viewFile({ path: `.rw-chatbot/CODEBASE_TOC.md` })}
+          className="text-sm font-semibold leading-6 text-green-600"
+        >
+          View Complete Codebase <span aria-hidden="true">→</span>
+        </Link>
+      </div>
+    </>
+  )
 }
